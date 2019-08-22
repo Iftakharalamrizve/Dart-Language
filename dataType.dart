93e7bab36169f5ced5da36d740c9fc54fe686979
Iftakharalamrizve <iftakharalam32@gmailcom>
2019-08-22 17:17:19 +0600

    Update datatype dart

---
+++ dataType.dart
@@ -0,0 +1,20 @@
+void main() {
+  int number = 10;
+  var snumber=10;
+  
+  
+  double dnumber1 =3.565455656;
+  var dnumber2=3.565455656;
+  
+  String name1="Google";
+  var name2="Microsoft";
+  
+  
+  bool valid=true;
+  var invalid=true;
+  
+  
+  //print hexa number 
+  int hexValue = 0xEADEBAEE;
+    
+}
